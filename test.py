bc6d753473952e268f66b5e849a686b639695320
Michael Ndon <manj19.mjn@gmail.com>
2020-12-23 18:30:49 +0100

    Model Created

--- test.py
+++ test.py
@@ -14,6 +14,15 @@ predict = "G3"
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
+# create test data
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-x_test, x_train, y_test, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+# create model and determine accuracy
+linear = linear_model.LinearRegression()
 
+linear.fit(x_train, y_train)
+acc = linear.score(x_test, y_test)
+print(acc)
+
+print("Coefficient: ", linear.coef_)
+print("Intercept: ", linear.intercept_)
