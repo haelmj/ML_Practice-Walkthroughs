83a1824783fec1021f569e97f2111a0a1ed8e9d0
Michael Ndon <manj19.mjn@gmail.com>
2020-12-28 10:44:37 +0100

    Plot data

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
-from sklearn.utils import shuffle
 import matplotlib.pyplot as pyplot
 import pickle
 from matplotlib import style
@@ -18,7 +17,7 @@ x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 # create test and train data
-'''
+
 best = 0
 for _ in range(50):
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
@@ -34,7 +33,7 @@ for _ in range(50):
         best = acc
         with open('studentmodel.pickle', 'wb') as f:
             pickle.dump(linear, f)
-'''
+
 with open('studentmodel.pickle', 'rb') as pickle_in:
     linear = pickle.load(pickle_in)
 
@@ -47,3 +46,10 @@ predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+
+p = 'G1'
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
