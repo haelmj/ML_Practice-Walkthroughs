c9f3bab24fed92f39509245422df4877e1e5e64f
Michael Ndon <manj19.mjn@gmail.com>
2020-12-28 10:37:56 +0100

    Feature: Save the highest achieved accuracy

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 
 # load data set into program
 data = pd.read_csv("student-mat.csv", sep=";")
@@ -13,16 +16,28 @@ predict = "G3"
 
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
-
-# create test and train data
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+# create test and train data
+'''
+best = 0
+for _ in range(50):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-# create model and determine accuracy
-linear = linear_model.LinearRegression()
+    # create model and determine accuracy
+    linear = linear_model.LinearRegression()
 
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-print(acc)
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print(acc)
+
+    if acc > best:
+        best = acc
+        with open('studentmodel.pickle', 'wb') as f:
+            pickle.dump(linear, f)
+'''
+with open('studentmodel.pickle', 'rb') as pickle_in:
+    linear = pickle.load(pickle_in)
+
 
 print("Coefficient: ", linear.coef_)
 print("Intercept: ", linear.intercept_)
